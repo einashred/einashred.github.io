7f2c1007ba67bac6ccb03ae2e25deffc48f217c3
stefaniemoessler <stefanie.moessler@gmx.net>
2018-04-18 18:08:06 +0200

    4 karten eingefuegt

--- basemap.js
+++ basemap.js
@@ -1,5 +1,35 @@
-let myMap = L.map("mapdiv");
-let myLayer = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png");
+let myMap = L.map("mapdiv"); 
+//spricht leaflet bib an erstellt variable myMap, da ist der link zur bib drin, erstellt neue Karte link auf html div //
 
-myMap.addLayer(myLayer);
+let myLayers = {
+    
+    osm : L.tileLayer(
+        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    ),
+    geolandbasemap : L.tileLayer(
+        "https://maps.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png"
+    ),
+    bmapoverlay : L.tileLayer(
+        "https://maps.wien.gv.at/basemap/bmapoverlay/normal/google3857/{z}/{y}/{x}.png"
+    ),
+    bmaphidpi : L.tileLayer(
+        "https://maps.wien.gv.at/basemap/bmaphidpi/normal/google3857/{z}/{y}/{x}.jpeg"
+    ),
+    bmapgrau : L.tileLayer(
+        "https://maps.wien.gv.at/basemap/bmapgrau/normal/google3857/{z}/{y}/{x}.png"
+    ),
+    bmaporthofoto30cm : L.tileLayer(
+        "https://maps.wien.gv.at/basemap/bmaporthofoto30cm/normal/google3857/{z}/{y}/{x}.jpeg"
+    ),
+    }
+//neuer layer wird erzeugt und über seite aufgerufen, geschwungen für platzhalter, z zoomstufe 11, x koordinate laengengrad, y breitengrad, s subdomain, //
+
+//let url = "https://maps.wien.gv.at/basemap/bmaphidpi/normal/google3857/{z}/{y}/{x}.png");
+
+
+
+
+
+myMap.addLayer(myLayers.bmapgrau);
 myMap.setView([47.267,11.383],11);
+//fuegt koordinaten ein, für die variablen//
