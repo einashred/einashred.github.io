11a7d5bf4ad379923c20d826f6b4e18e3dece1fe
stefaniemoessler <stefanie.moessler@gmx.net>
2018-04-18 18:21:40 +0200

    subdomains hinzugefuegt

--- basemap.js
+++ basemap.js
@@ -7,19 +7,34 @@ let myLayers = {
         "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     ),
     geolandbasemap : L.tileLayer(
-        "https://maps.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png"
+        "https://maps.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png" , {
+        subdomains : ["maps","maps2","maps2","maps3","maps4"],
+        attribution : "Datenquelle: <a href='https://www.basemap.at'>basematp.at</a>"
+    }
     ),
     bmapoverlay : L.tileLayer(
-        "https://maps.wien.gv.at/basemap/bmapoverlay/normal/google3857/{z}/{y}/{x}.png"
+        "https://maps.wien.gv.at/basemap/bmapoverlay/normal/google3857/{z}/{y}/{x}.png", {
+            subdomains : ["maps","maps2","maps2","maps3","maps4"],
+            attribution : "Datenquelle: <a href='https://www.basemap.at'>basematp.at</a>"
+        }
     ),
     bmaphidpi : L.tileLayer(
-        "https://maps.wien.gv.at/basemap/bmaphidpi/normal/google3857/{z}/{y}/{x}.jpeg"
+        "https://maps.wien.gv.at/basemap/bmaphidpi/normal/google3857/{z}/{y}/{x}.jpeg", {
+            subdomains : ["maps","maps2","maps2","maps3","maps4"],
+            attribution : "Datenquelle: <a href='https://www.basemap.at'>basematp.at</a>"
+        }
     ),
     bmapgrau : L.tileLayer(
-        "https://maps.wien.gv.at/basemap/bmapgrau/normal/google3857/{z}/{y}/{x}.png"
+        "https://maps.wien.gv.at/basemap/bmapgrau/normal/google3857/{z}/{y}/{x}.png", {
+            subdomains : ["maps","maps2","maps2","maps3","maps4"],
+            attribution : "Datenquelle: <a href='https://www.basemap.at'>basematp.at</a>"
+        }
     ),
     bmaporthofoto30cm : L.tileLayer(
-        "https://maps.wien.gv.at/basemap/bmaporthofoto30cm/normal/google3857/{z}/{y}/{x}.jpeg"
+        "https://maps.wien.gv.at/basemap/bmaporthofoto30cm/normal/google3857/{z}/{y}/{x}.jpeg", {
+            subdomains : ["maps","maps2","maps2","maps3","maps4"],
+            attribution : "Datenquelle: <a href='https://www.basemap.at'>basematp.at</a>"
+        }
     ),
     }
 //neuer layer wird erzeugt und über seite aufgerufen, geschwungen für platzhalter, z zoomstufe 11, x koordinate laengengrad, y breitengrad, s subdomain, //
@@ -30,6 +45,6 @@ let myLayers = {
 
 
 
-myMap.addLayer(myLayers.bmapgrau);
+myMap.addLayer(myLayers.bmaporthofoto30cm);
 myMap.setView([47.267,11.383],11);
 //fuegt koordinaten ein, für die variablen//
